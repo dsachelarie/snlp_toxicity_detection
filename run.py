a4a5405ce74207ca6f691386b0576b48a8235de8
jadereinila <jade.reinila@student.lut.fi>
2024-04-12 14:30:19 +0300

    modified bert, created ensemble prediction code

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
 from models.basic_model import BasicModel
 from models.bert_model import BertModel
+from utils import write_ensemble_preds
 
 
 if __name__ == "__main__":
-    model = BasicModel(mode="debug", preprocessing="glove_rtf_igm")
+    #model = BasicModel(mode="debug", preprocessing="glove_rtf_igm")
     # model = BertModel(preprocessing="glove_rtf_igm")
     # model = BertModel()
 
-    model.run()
+    #model.run()
+
+    # Aggregating ensemble predictions
+    file_names = ["data/preds_roberta_5k_submitted.csv", "data/preds_alexnet.csv", "data/preds_svm.csv"]
+    write_ensemble_preds(file_names)
--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ from nltk.corpus import stopwords
 import pandas as pd
 import csv
 import math
+from collections import Counter
 
 nltk.download("stopwords")
 
@@ -214,3 +215,37 @@ def read_prob_weights_cached(file: str):
         prob_per_word[df["word"][i]] = df["weight"][i]
 
     return prob_per_word
+
+def write_ensemble_preds(file_names: list):
+
+    all_preds = []
+
+    # Reading predictions from each file
+    for file_name in file_names:
+        with open(file_name, 'r') as file:
+            reader = csv.reader(file)
+            next(reader)
+            predictions = [int(row[1]) for row in reader]
+            all_preds.append(predictions)
+
+    print("Writing predictions to file")
+
+    with open("data/ensemble_preds.csv", "w", newline="") as f:
+        writer = csv.DictWriter(f, fieldnames=["id", "label"])
+
+        writer.writeheader()
+
+        for i, preds in enumerate(zip(*all_preds)):
+
+            #data_point_preds = [preds[0][i], preds[1][i], preds[2][i]]
+            most_common_prediction = max(set(preds), key=preds.count)
+            writer.writerow({"id": i, "label": most_common_prediction})
+
+
+    #ensemble_preds = []
+    # Selecting the most common prediction for each data point
+    #for predictions in zip(*all_preds):
+    #    most_common_prediction = Counter(predictions).most_common(1)[0][0]
+    #    ensemble_preds.append(most_common_prediction)
+
+    
